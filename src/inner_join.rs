964353a03a5dfd6b0d6becc71e4a929b0f1e9ffd
Colin Sherratt <colin.sherratt@gmail.com>
2014-11-29 23:07:01 -0500

    merge inner and outer join

--- src/inner_join.rs
+++ src/inner_join.rs
@@ -2,6 +2,7 @@
 /// ordered iterators. This can be useful to do selects between multiple
 /// tables of items
 
+use std::iter::Peekable;
 use std::collections::{
     btree_map, btree_set,
     tree_map, tree_set,
@@ -13,9 +14,9 @@ use std::collections::{
 /// Allows an iterator to be do an inner join with another
 /// iterator to combine their values or filter based on their keys.
 /// this trait is applied to an iterator over a map like structure
-pub trait InnerJoinMap<K, A>: Iterator<(K, A)> {
+pub trait OrderedMapIterator<K, A>: Iterator<(K, A)> {
     /// join two ordered maps together
-    fn inner_join_map<B, T: InnerJoinMap<K, B>>(self, map: T)
+    fn inner_join_map<B, T: OrderedMapIterator<K, B>>(self, map: T)
         -> InnerJoinMapIterator<Self, T> {
         InnerJoinMapIterator {
             a: self,
@@ -24,11 +25,26 @@ pub trait InnerJoinMap<K, A>: Iterator<(K, A)> {
     }
 
     /// filter an ordered map with an ordered set
-    fn inner_join_set<B, T: InnerJoinSet<K>>(self, set: T)
+    fn inner_join_set<B, T: OrderedSetIterator<K>>(self, set: T)
         -> InnerJoinMapSetIterator<Self, T> {
         InnerJoinMapSetIterator {
             map: self,
             set: set
+        }
+    }
+
+    /// Join an ordered iterator with the right ordered iterator. The
+    /// new iterator will return a key value pair for every key in
+    /// either iterator. If a key is present in both iterators they
+    /// will be returned together (two values). If a value is in the Right,
+    /// but not the left iterator it will be return without the value in the
+    /// left iterator. If the value is in the left iterator by not the right
+    /// that will be return without the value from the left iterator.
+    fn outer_join<B, T: OrderedMapIterator<K, B>>(self, right: T)
+        -> OuterJoinIterator<Self, T, (K, A), (K, B)> {
+        OuterJoinIterator {
+            left: self.peekable(),
+            right: right.peekable()
         }
     }
 }
@@ -36,9 +52,9 @@ pub trait InnerJoinMap<K, A>: Iterator<(K, A)> {
 /// Allows an iterator to be do an inner join with another
 /// iterator to combine their values or filter based on their keys.
 /// this trait is applied to an iterator over a set like structure
-pub trait InnerJoinSet<K>: Iterator<K> {
+pub trait OrderedSetIterator<K>: Iterator<K> {
     /// join two ordered maps together
-    fn inner_join_map<B, T: InnerJoinMap<K, B>>(self, map: T)
+    fn inner_join_map<B, T: OrderedMapIterator<K, B>>(self, map: T)
         -> InnerJoinMapSetIterator<T, Self> {
         InnerJoinMapSetIterator {
             map: map,
@@ -47,7 +63,7 @@ pub trait InnerJoinSet<K>: Iterator<K> {
     }
 
     /// filter an ordered map with an ordered set
-    fn inner_join_set<T: InnerJoinSet<K>>(self, map: T)
+    fn inner_join_set<T: OrderedSetIterator<K>>(self, map: T)
         -> InnerJoinSetIterator<Self, T> {
         InnerJoinSetIterator {
             a: self,
@@ -59,10 +75,14 @@ pub trait InnerJoinSet<K>: Iterator<K> {
 pub struct InnerJoinMapIterator<A, B> {a: A, b: B}
 pub struct InnerJoinMapSetIterator<A, B> {map: A, set: B}
 pub struct InnerJoinSetIterator<A, B> {a: A, b: B}
+pub struct OuterJoinIterator<A, B, VA, VB> {
+    left: Peekable<VA, A>,
+    right: Peekable<VB, B>
+}
 
 impl<K: Ord, DataA, DataB,
-     A: InnerJoinMap<K, DataA>,
-     B: InnerJoinMap<K, DataB>>
+     A: OrderedMapIterator<K, DataA>,
+     B: OrderedMapIterator<K, DataB>>
      Iterator<(K, (DataA, DataB))> for InnerJoinMapIterator<A, B> {
 
     fn next(&mut self) -> Option<(K, (DataA, DataB))> {
@@ -104,8 +124,8 @@ impl<K: Ord, DataA, DataB,
 
 
 impl<K: Ord,
-     A: InnerJoinSet<K>,
-     B: InnerJoinSet<K>>
+     A: OrderedSetIterator<K>,
+     B: OrderedSetIterator<K>>
      Iterator<K> for InnerJoinSetIterator<A, B> {
     fn next(&mut self) -> Option<K> {
         let mut key_a = match self.a.next() {
@@ -140,8 +160,8 @@ impl<K: Ord,
 
 impl<K: Ord,
      V,
-     SetIter: InnerJoinSet<K>,
-     MapIter: InnerJoinMap<K, V>>
+     SetIter: OrderedSetIterator<K>,
+     MapIter: OrderedMapIterator<K, V>>
      Iterator<(K, V)> for InnerJoinMapSetIterator<MapIter, SetIter> {
     fn next(&mut self) -> Option<(K, V)> {
         let mut key_set = match self.set.next() {
@@ -177,25 +197,59 @@ impl<K: Ord,
     }
 }
 
-impl<'a, K: Ord> InnerJoinSet<&'a K> for btree_set::Items<'a, K> {}
-impl<'a, K: Ord, V> InnerJoinMap<&'a K, &'a V> for btree_map::Entries<'a, K, V> {}
-impl<'a, K: Ord> InnerJoinSet<&'a K> for tree_set::SetItems<'a, K> {}
-impl<'a, K: Ord, V> InnerJoinMap<&'a K, &'a V> for tree_map::Entries<'a, K, V> {}
-impl<'a> InnerJoinSet<uint> for trie_set::SetItems<'a> {}
-impl<'a, V> InnerJoinMap<uint, &'a V> for trie_map::Entries<'a, V> {}
-impl<'a, V> InnerJoinMap<uint, &'a V> for vec_map::Entries<'a, V> {}
-impl<'a> InnerJoinSet<uint> for bitv_set::BitPositions<'a> {}
+impl<K: Ord+Eq,
+     VA, VB,
+     A: OrderedMapIterator<K, VA>,
+     B: OrderedMapIterator<K, VB>>
+     Iterator<(K, (Option<VA>, Option<VB>))> for OuterJoinIterator<A, B, (K, VA), (K, VB)> {
+
+    fn next(&mut self) -> Option<(K, (Option<VA>, Option<VB>))> {
+        let which = match (self.left.peek(), self.right.peek()) {
+            (Some(&(ref ka, _)), Some(&(ref kb, _))) => kb.cmp(ka),
+            (None, Some(_)) => Less,
+            (Some(_), None) => Greater,
+            (None, None) => return None
+        };
+
+        match which {
+            Equal => {
+                let ((k, a), (_, b)) =
+                    (self.left.next().expect("no value found"),
+                     self.right.next().expect("no value found"));
+
+                Some((k, (Some(a), Some(b))))
+            }
+            Less => {
+                let (k, v) = self.right.next().expect("no value found");
+                Some((k, (None, Some(v))))
+            }
+            Greater => {
+                let (k, v) = self.left.next().expect("no value found");
+                Some((k, (Some(v), None)))
+            }
+        }
+    }
+}
+
+impl<'a, K: Ord> OrderedSetIterator<&'a K> for btree_set::Items<'a, K> {}
+impl<'a, K: Ord, V> OrderedMapIterator<&'a K, &'a V> for btree_map::Entries<'a, K, V> {}
+impl<'a, K: Ord> OrderedSetIterator<&'a K> for tree_set::SetItems<'a, K> {}
+impl<'a, K: Ord, V> OrderedMapIterator<&'a K, &'a V> for tree_map::Entries<'a, K, V> {}
+impl<'a> OrderedSetIterator<uint> for trie_set::SetItems<'a> {}
+impl<'a, V> OrderedMapIterator<uint, &'a V> for trie_map::Entries<'a, V> {}
+impl<'a, V> OrderedMapIterator<uint, &'a V> for vec_map::Entries<'a, V> {}
+impl<'a> OrderedSetIterator<uint> for bitv_set::BitPositions<'a> {}
 
 
 impl<K: Ord, VA, VB,
-     A: InnerJoinMap<K, VA>,
-     B: InnerJoinMap<K, VB>> InnerJoinMap<K, (VA, VB)> for InnerJoinMapIterator<A, B> {}
+     A: OrderedMapIterator<K, VA>,
+     B: OrderedMapIterator<K, VB>> OrderedMapIterator<K, (VA, VB)> for InnerJoinMapIterator<A, B> {}
 impl<K: Ord, V,
-     A: InnerJoinSet<K>,
-     B: InnerJoinMap<K, V>> InnerJoinMap<K, V> for InnerJoinMapSetIterator<B, A> {}
+     A: OrderedSetIterator<K>,
+     B: OrderedMapIterator<K, V>> OrderedMapIterator<K, V> for InnerJoinMapSetIterator<B, A> {}
 impl<K: Ord,
-     A: InnerJoinSet<K>,
-     B: InnerJoinSet<K>> InnerJoinSet<K> for InnerJoinSetIterator<A, B> {}
+     A: OrderedSetIterator<K>,
+     B: OrderedSetIterator<K>> OrderedSetIterator<K> for InnerJoinSetIterator<A, B> {}
 
 #[test]
 fn join_two_sets() {
@@ -267,4 +321,37 @@ fn join_two_maps_to_set() {
     assert_eq!(Some((12, 4)), powers_of_two_and_three.next())
     assert_eq!(Some((18, 6)), powers_of_two_and_three.next())
     assert_eq!(None, powers_of_two_and_three.next())
+}
+
+#[test]
+fn outer_join_fizz_buzz() {
+    use std::collections::BTreeMap;
+
+    let mul_of_three: BTreeMap<int, int> = range(0, 100).map(|x| (x*3, x)).collect();
+    let mul_of_five: BTreeMap<int, int> = range(0, 100).map(|x| (x*5, x)).collect();
+
+    let mut fizz_buzz = BTreeMap::new();
+
+    for (key, (three, five)) in mul_of_three.iter()
+                                            .outer_join(mul_of_five.iter()) {
+        fizz_buzz.insert(key, (three.is_some(), five.is_some()));
+    }
+
+    let res: BTreeMap<int, String> = range(1, 100).map(|i|
+        (i, match fizz_buzz.get(&i) {
+            None => format!("{}", i),
+            Some(&(true, false)) => format!("Fizz"),
+            Some(&(false, true)) => format!("Buzz"),
+            Some(&(true, true)) => format!("FizzBuzz"),
+            Some(&(false, false)) => panic!("Outer join failed...")
+        })).collect();
+
+    for i in range(1, 100) {
+        match (i % 3, i % 5) {
+            (0, 0) => assert_eq!("FizzBuzz", res[i].as_slice()),
+            (0, _) => assert_eq!("Fizz", res[i].as_slice()),
+            (_, 0) => assert_eq!("Buzz", res[i].as_slice()),
+            _ => assert_eq!(format!("{}", i).as_slice(), res[i].as_slice())
+        }
+    }
 }
