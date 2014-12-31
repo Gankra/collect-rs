f70d4f1b3df619d1f38257754feb0c398600c6e5
Jonathan S <gereeter@gmail.com>
2014-12-31 11:52:52 -0600

    Added first allocator

--- src/alloc/mod.rs
+++ src/alloc/mod.rs
@@ -1,11 +1,38 @@
 pub use self::mem_descriptor::MemDescriptor;
 
+use rust_alloc::heap;
+
 pub mod mem_descriptor;
 
 pub trait Allocator {
-    fn allocate(&mut self, descriptor: MemDescriptor) -> *mut u8;
-    fn deallocate(&mut self, descriptor: MemDescriptor);
-    fn shrink(&mut self, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool;
-    fn grow(&mut self, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool;
+    unsafe fn allocate(&mut self, descriptor: MemDescriptor) -> *mut u8;
+    unsafe fn deallocate(&mut self, ptr: *mut u8, descriptor: MemDescriptor);
+    unsafe fn shrink(&mut self, ptr: *mut u8, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool;
+    unsafe fn grow(&mut self, ptr: *mut u8, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool;
     fn usable_size(&self, descriptor: MemDescriptor) -> uint;
+}
+
+#[deriving(Copy, Clone)]
+pub struct RustAllocator;
+
+impl Allocator for RustAllocator {
+    unsafe fn allocate(&mut self, descriptor: MemDescriptor) -> *mut u8 {
+        heap::allocate(descriptor.size, descriptor.alignment)
+    }
+
+    unsafe fn deallocate(&mut self, ptr: *mut u8, descriptor: MemDescriptor) {
+        heap::deallocate(ptr, descriptor.size, descriptor.alignment)
+    }
+
+    unsafe fn shrink(&mut self, ptr: *mut u8, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool {
+        heap::reallocate_inplace(ptr, old_descriptor.size, new_descriptor.size, old_descriptor.alignment) == self.usable_size(new_descriptor)
+    }
+
+    unsafe fn grow(&mut self, ptr: *mut u8, old_descriptor: MemDescriptor, new_descriptor: MemDescriptor) -> bool {
+        heap::reallocate_inplace(ptr, old_descriptor.size, new_descriptor.size, old_descriptor.alignment) == self.usable_size(new_descriptor)
+    }
+
+    fn usable_size(&self, descriptor: MemDescriptor) -> uint {
+        heap::usable_size(descriptor.size, descriptor.alignment)
+    }
 }
